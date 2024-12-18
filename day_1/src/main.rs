ac7c79a8f92d25479d36d2d5f7ad4db23024a350
Blue <108904799+UnintelligableGoofball@users.noreply.github.com>
2024-12-18 08:07:37 +0000

    figured out fukkin files yeeeehaw

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let input_file: &str = "day_1_input.txt";
+    let data_string: String = file_to_string(input_file);
+    print!("{} conttains:\n{}\n", input_file, data_string);
+}
+
+fn file_to_string(filename: &str) -> String {
+    // Create a path to the desired file
+    let path = Path::new(filename);
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(s) => s,
+    };
+
+    return s;
+
+    // `file` goes out of scope, and the "hello.txt" file gets closed
 }
